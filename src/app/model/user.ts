55e7b0bfb67ae4ba7f0dd3c32767b8fa09bd81fb
Valentin Piho <val.piho@gmail.com>
2020-12-29 20:36:16 +0200

    Updating user model

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,2 +1,27 @@
 export class User {
+  public id: number;
+  public userId: string;
+  public firstName: string;
+  public lastName: string;
+  public username: string;
+  public email: string;
+  public logInDateDisplay: Date;
+  public joinDate: Date;
+  public profileImageUrl: string;
+  public active: boolean;
+  public notLocked: boolean;
+  public role: string;
+  public authorities: [];
+
+
+  constructor() {
+    this.firstName = '';
+    this.lastName = '';
+    this.email = '';
+    this.username = '';
+    this.active = false;
+    this.notLocked = false;
+    this.role = '';
+    this.authorities = [];
+  }
 }
