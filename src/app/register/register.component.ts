7497ac860ea187eedd025f139118e152bb16be04
Valentin Piho <val.piho@gmail.com>
2021-01-05 22:57:40 +0200

    Registration

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AuthenticationService} from "../service/authentication.service";
+import {Router} from "@angular/router";
+import {NotificationService} from "../service/notification.service";
+import {User} from "../model/user";
+import {HttpErrorResponse} from "@angular/common/http";
+import {NotificationType} from "../enum/notification-type.enum";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
+  public showLoading: boolean;
+  private subscriptions: Subscription[] = [];
 
-  constructor() { }
+  constructor(private router: Router,
+              private authenticationService: AuthenticationService,
+              private notificationService: NotificationService) { }
 
   ngOnInit(): void {
+    if (this.authenticationService.isUserLoggedIn()) {
+      this.router.navigateByUrl('/user/management');
+    }
   }
 
+  public onRegister(user: User): void {
+    this.showLoading = true;
+    this.subscriptions.push(
+      this.authenticationService.register(user).subscribe(
+        (response: User) => {
+          this.showLoading = false;
+          this.sendNotification(NotificationType.SUCCESS, `A new account was created for ${response.firstName}.
+          Please check your email for password to log in.`);
+        },
+        (errorResponse: HttpErrorResponse) => {
+          console.log(errorResponse);
+          this.sendNotification(NotificationType.ERROR, errorResponse.error.message);
+          this.showLoading = false;
+        }
+      )
+    )
+  }
+
+  ngOnDestroy(): void {
+  }
+
+  private sendNotification(notificationType: NotificationType, message: string) {
+    if (message) {
+      this.notificationService.notify(notificationType, message);
+    } else {
+      this.notificationService.notify(notificationType, 'AN ERROR OCCURRED. PLEASE TRY AGAIN');
+    }
+  }
 }
