364c228b66733e33913e23e3a97b43910fa990b6
Valentin Piho <val.piho@gmail.com>
2021-01-07 11:45:00 +0200

    List of users

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import {BehaviorSubject} from "rxjs";
+import {Component, OnInit} from '@angular/core';
+import {BehaviorSubject, Subscription} from "rxjs";
+import {User} from "../model/user";
+import {UserService} from "../service/user.service";
+import {NotificationService} from "../service/notification.service";
+import {NotificationType} from "../enum/notification-type.enum";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-user',
@@ -9,14 +14,47 @@ import {BehaviorSubject} from "rxjs";
 export class UserComponent implements OnInit {
   private titleSubject = new BehaviorSubject<string>('Users');
   public titleAction$ = this.titleSubject.asObservable();
+  public refreshing: boolean;
 
-  constructor() { }
+  public users: User[]
+  private subscriptions: Subscription[] = [];
+
+  constructor(private userService: UserService,
+              private notificationService: NotificationService) { }
+
+  ngOnInit(): void {
+    this.getUsers(true);
+  }
 
   public changeTitle(title: string): void {
     this.titleSubject.next(title);
   }
 
-  ngOnInit(): void {
+  public getUsers(showNotification: boolean): void {
+    this.refreshing = true;
+    this.subscriptions.push(
+      this.userService.getUsers().subscribe(
+        (response: User[]) => {
+          this.userService.addUsersToLocalCache(response);
+          this.users = response;
+          this.refreshing = false;
+          if (showNotification) {
+            this.sendNotification(NotificationType.SUCCESS, `${response.length} user(s) loaded successfully`)
+          }
+        },
+        (errorResponse: HttpErrorResponse) => {
+          this.sendNotification(NotificationType.ERROR, errorResponse.error.message)
+          this.refreshing = false;
+        }
+      )
+    );
   }
 
+  private sendNotification(notificationType: NotificationType, message: string) {
+    if (message) {
+      this.notificationService.notify(notificationType, message);
+    } else {
+      this.notificationService.notify(notificationType, 'AN ERROR OCCURRED. PLEASE TRY AGAIN');
+    }
+  }
 }
